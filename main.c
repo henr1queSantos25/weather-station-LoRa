ad7a62d2ceeda797881b6637ee8cd31668650f32
Dom-Rodrigo <rapiropo.cic@uesc.br>
2025-08-09 16:02:17 -0300

    Transmite dados do sensor vira LoRa

--- main.c
+++ main.c
@@ -6,10 +6,28 @@
 #include "bmp280.h"
 #include "aht20.h"
 
+// === DEFINIÇÕES DE PINOS E CONSTANTES ===
+#define I2C_PORT_SENSORS i2c0
+#define I2C_SDA_SENSORS 0
+#define I2C_SCL_SENSORS 1
+#define I2C_PORT_DISP i2c1
+#define I2C_SDA_DISP 14
+#define I2C_SCL_DISP 15
 
+// === DADOS DOS SENSORES ===
+float temperatura;
+int32_t pressao;
+float umidade;
 
 int main() {
     stdio_init_all();
+
+    // === CONFIGURAÇÃO DOS SENSORES ===
+    setup_I2C_aht20(I2C_PORT_SENSORS, I2C_SDA_SENSORS, I2C_SCL_SENSORS, 400 * 1000);
+    aht20_reset(I2C_PORT_SENSORS);
+    aht20_init(I2C_PORT_SENSORS);
+    bmp280_init(I2C_PORT_SENSORS);
+
 
     if(!rfm95_initialize()) {
         printf("RFM95 initialization failed!\n");
@@ -21,9 +39,39 @@ int main() {
 
 
     char buffer[64];
+    
+    AHT20_Data data;
+    int32_t raw_temp_bmp;
+    int32_t raw_pressure;
+
+    // Inicialização das estruturas de dados para os sensores
+    struct bmp280_calib_param params;
+    bmp280_get_calib_params(I2C_PORT_SENSORS, &params);
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+
+
+        // === LEITURA DO SENSOR BMP280 ===
+        bmp280_read_raw(I2C_PORT_SENSORS, &raw_temp_bmp, &raw_pressure);
+        int32_t temperature_bmp = bmp280_convert_temp(raw_temp_bmp, &params); 
+        pressao = (bmp280_convert_pressure(raw_pressure, raw_temp_bmp, &params) / 1000); // kPa
+
+        // === LEITURA DO SENSOR AHT20 ===
+        if (aht20_read(I2C_PORT_SENSORS, &data)) {
+            temperatura = ((data.temperature + (temperature_bmp / 100.0)) / 2.0); // Média das temperaturas
+            umidade = (data.humidity) > 100 ? 100 : (data.humidity); // Limita a umidade a 100%
+        }
+        else {
+            printf("Erro ao ler AHT20\n");
+            temperatura = 0.0;
+            umidade = 0.0;
+        }
+
+        snprintf(buffer, sizeof(buffer),
+                            "{\"temperatura\":%.2f,\"pressao\":%d,\"umidade\":%.2f}\r\n",
+                            temperatura, pressao, umidade);
+        rfm95_transmit((uint8_t*)buffer, strlen(buffer));
+
+        sleep_ms(2000);
     }
 }
